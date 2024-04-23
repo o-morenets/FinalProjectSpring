def3b3992edbc06e74188bc4a558e8c7c519fabb
Oleksii Morenets <oleksii.morenets@gmail.com>
2024-04-23 18:08:30 +0200

    unused import removed

--- src/main/java/ua/training/admission/repository/SubjectGradeRepository.java
+++ src/main/java/ua/training/admission/repository/SubjectGradeRepository.java
@@ -4,9 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
-import ua.training.admission.entity.Subject;
 import ua.training.admission.entity.SubjectGrade;
-import ua.training.admission.entity.User;
 
 import java.util.List;
 
