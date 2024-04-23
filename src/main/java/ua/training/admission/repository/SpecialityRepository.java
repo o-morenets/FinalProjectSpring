37c11cbbc953329fd6472ef5b163eb3f1cb34eeb
Oleksii Morenets <oleksii.morenets@gmail.com>
2024-04-23 18:09:08 +0200

    prevent N+1 query problem

--- src/main/java/ua/training/admission/repository/SpecialityRepository.java
+++ src/main/java/ua/training/admission/repository/SpecialityRepository.java
@@ -1,10 +1,15 @@
 package ua.training.admission.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import ua.training.admission.entity.Speciality;
+
+import java.util.List;
 
 @Repository
 public interface SpecialityRepository extends JpaRepository<Speciality, Long> {
 
+    @Query("select distinct spec from Speciality spec join fetch spec.subjects")
+    List<Speciality> findAll();
 }
