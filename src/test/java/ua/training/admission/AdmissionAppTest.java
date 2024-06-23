700186bfbeb5479a7b905e781e320fe1ea224521
Oleksii Morenets <oleksii.morenets@gmail.com>
2024-06-23 12:08:31 +0200

    Test added

---
+++ src/test/java/ua/training/admission/AdmissionAppTest.java
@@ -0,0 +1,12 @@
+package ua.training.admission;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdmissionAppTest {
+
+    @Test
+    void contextLoads() {
+    }
+}
